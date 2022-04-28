d245aa20ce3a66a1b75bc73739a5cc696abf07a8
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-04-28 20:25:09 +0100

    Project created

---
+++ src/main/java/com/educandoweb/Aula61springJPAHibernate/Aula61SpringJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Aula61springJPAHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula61SpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula61SpringJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/Aula61springJPAHibernate/Aula61SpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Aula61springJPAHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula61SpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
