417366203da679ba7a16d9030b893895e7bd8a97
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-05-03 18:54:55 +0100

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/Aula61springJPAHibernate/entities/Product.java
+++ src/main/java/com/educandoweb/Aula61springJPAHibernate/entities/Product.java
@@ -12,7 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -37,6 +40,9 @@ public class Product implements Serializable{
 	joinColumns = @JoinColumn(name = "product_id"),
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 	}
@@ -91,6 +97,18 @@ public class Product implements Serializable{
 
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){	
+		Set<Order> set = new HashSet<>();
+	
+		// Percorre o set de OrderItem, e adiciona em um novo set, apenas os objetos do tipo
+		// Order.
+		for (OrderItem oi : items) {  
+			set.add(oi.getOrder());
+		}
+		return set;
 	}
 
 	@Override
