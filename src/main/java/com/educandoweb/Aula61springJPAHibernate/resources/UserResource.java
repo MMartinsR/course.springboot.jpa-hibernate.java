e270899145c2708faf9c4768458d3148141787c7
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-05-03 22:58:34 +0100

    User delete

--- src/main/java/com/educandoweb/Aula61springJPAHibernate/resources/UserResource.java
+++ src/main/java/com/educandoweb/Aula61springJPAHibernate/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,14 @@ public class UserResource {
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
 		
+	}
+	
+	// A requisição HTTP referente a deleção de um objeto/dado, é o @DeleteMapping.
+	// Já que a ResponseEntity desta vez não retornará nada, devemos passar o valor Void para
+	// ela.
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
