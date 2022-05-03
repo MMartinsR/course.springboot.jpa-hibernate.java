d751153f869910032f9ff4a36bd86f7c073abce4
Mariana Martins <mariana.martins.rosa.mmr@gmail.com>
2022-05-03 02:15:00 +0100

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/Aula61springJPAHibernate/entities/Product.java
+++ src/main/java/com/educandoweb/Aula61springJPAHibernate/entities/Product.java
@@ -9,8 +9,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -26,7 +28,14 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
+	// Anotação muitos para muitos
+	// Anotação de join, para unir as tabelas, criando um tabela de referência as duas
+	// dando o nome de tb_product_category, onde a chave estrangeira de produto terá nome de
+	// product_id e a de categoria tera nome de category_id.
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
